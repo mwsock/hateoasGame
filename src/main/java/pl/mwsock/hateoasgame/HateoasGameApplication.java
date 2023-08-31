4117b4296b4d52b1e2c4f62ba3f5169b9fe29069
mwsock <mwsock93@gmail.com>
2023-08-31 23:09:12 +0200

    git init

---
+++ src/main/java/pl/mwsock/hateoasgame/HateoasGameApplication.java
@@ -0,0 +1,13 @@
+package pl.mwsock.hateoasgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HateoasGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mwsock/hateoasgame/HateoasGameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwsock.hateoasgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
