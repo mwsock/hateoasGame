c4c98de4217eb761cf5a242fe1bf794c354bb51f
mwsock <mwsock93@gmail.com>
2023-09-03 12:53:46 +0200

    hateoas first usage

--- src/main/java/pl/mwsock/hateoasgame/greetings/GreetingsController.java
+++ src/main/java/pl/mwsock/hateoasgame/greetings/GreetingsController.java
@@ -1,28 +1,51 @@
 package pl.mwsock.hateoasgame.greetings;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.mwsock.hateoasgame.player.PlayerEntity;
+import pl.mwsock.hateoasgame.player.PlayerRepository;
 import pl.mwsock.hateoasgame.player.PlayerService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/")
 public class GreetingsController {
 
     private final PlayerService playerService;
+    private final PlayerRepository playerRepository;
 
-    public GreetingsController(PlayerService playerService) {
+    public GreetingsController(PlayerService playerService, PlayerRepository playerRepository) {
         this.playerService = playerService;
+        this.playerRepository = playerRepository;
     }
+
 
     @GetMapping
-    public String showGreetings(){
-        return "Greetings stranger!";
+    public ResponseEntity<List<PlayerEntity>> showGreetings(){
+        List<PlayerEntity> playerEntities = playerRepository.findAll();
+        for (PlayerEntity entity: playerEntities){
+            entity.add(linkTo(
+                    methodOn(GreetingsController.class).findPlayerById(entity.getId())
+            ).withSelfRel());
+        }
+        return new ResponseEntity<>(playerEntities, HttpStatus.OK);
     }
 
-    @GetMapping("/{playerName}")
-    public String showGreetingsWithName(@PathVariable String playerName){
-        return playerService.greetPlayer(playerName);
+    @GetMapping("/{id}")
+    public ResponseEntity<PlayerEntity> findPlayerById(@PathVariable Long id){
+        Optional<PlayerEntity> playerEntity = playerRepository.findById(id);
+        return new ResponseEntity<>(playerEntity.orElse(new PlayerEntity()),HttpStatus.OK);
     }
 }
--- src/main/java/pl/mwsock/hateoasgame/player/PlayerEntity.java
+++ src/main/java/pl/mwsock/hateoasgame/player/PlayerEntity.java
@@ -1,20 +1,45 @@
 package pl.mwsock.hateoasgame.player;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.Objects;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name ="playes")
-public class PlayerEntity {
+@Table(name ="players")
+public class PlayerEntity extends RepresentationModel<PlayerEntity> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @JsonCreator
+    public PlayerEntity(@JsonProperty("id") Long id,@JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PlayerEntity that = (PlayerEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, name);
+    }
 }
